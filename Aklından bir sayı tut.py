bc0920e2137ffb2fd3e737d77db902ad5a29f218
JavanessRqan <59340428+JavanessRqan@users.noreply.github.com>
2020-01-18 21:20:43 +0300

    Add files via upload

---
+++ Aklından bir sayı tut.py
@@ -0,0 +1,17 @@
+a = float(input("Aklınızdan 1 ile 20 arasında bir sayı tutun ve buraya yazın:"))
+b = float(input("Tuttuğunuz sayıyı 2 ile çarpın ve çıkan sonucu buraya yazın: "))
+c = float(input("Çıkan sonuca 10 ekleyin ve çıkan sonucu buraya yazın:"))
+d = float(input("Çıkan sonucu 2'ye böl ve çıkan sonucu buraya yazın:"))
+e = float(input("Çıkan sonucu en başta tuttuğunuz sayıdan çıkartın ve çıkan sonucu buraya yazın"))
+cevap = input("""
+    Cikan sonuc 5 ise 1 i degilse 2 yi secin.
+    
+    1)Evet
+    2)Hayır
+________________________________________________
+""")
+
+if cevap == "1":
+    print(" A W E S O M E ")
+else:
+    print("Ah be çok yaklaşmıştım.")
